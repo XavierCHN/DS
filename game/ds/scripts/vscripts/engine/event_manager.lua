c66064fe04bfc36853c357ca23375da9aff561ff
Xavier Tseng <Xavier Tseng>
2016-09-02 17:13:05 +0800

    finished event manager

--- game/ds/scripts/vscripts/engine/event_manager.lua
+++ game/ds/scripts/vscripts/engine/event_manager.lua
@@ -1,12 +1,18 @@
 if EventManager == nil then EventManager = class({}) end
 
 function EventManager:constructor()
+    self.all_events = {}
 end
 
 function EventManager:Emit(event_name, args)
-    
+    if self.all_events[event_name] then
+        for _, callback in pairs(self.all_events[event_name]) do
+            callback(args)
+        end
+    end
 end
 
 function EventManager:Register(event_name, callback)
-    
+    self.all_events[event_name] = self.all_events[event_name] or {}
+    table.insert(self.all_events[event_name], callback) 
 end
